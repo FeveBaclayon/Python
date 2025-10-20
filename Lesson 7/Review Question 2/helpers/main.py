fbd4c2637e807543c12baea47668bd887ecd9796
FeveBaclayon <lanzraijin@gmail.com>
2025-10-20 14:47:16 +0800

    Add files via upload

---
+++ Lesson 7/Review Question 2/helpers/main.py
@@ -0,0 +1,8 @@
+from helpers.string import shout
+from helpers.math import area
+
+length = 5
+width = 8
+result = area(length, width)
+
+print(shout(f"The area of a {length}-by-{width} rectangle is {result}"))
